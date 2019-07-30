5bc4c9f2e300111ea97e7c8d32de3b1696ced15d
xkorey <xkorey@gmail.com>
2019-07-30 21:39:23 +0800

    json转换设置

--- src/main/java/com/xkorey/subscribe/SubscribeApplication.java
+++ src/main/java/com/xkorey/subscribe/SubscribeApplication.java
@@ -1,8 +1,10 @@
 package com.xkorey.subscribe;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -18,6 +20,13 @@ public class SubscribeApplication {
 	RestTemplate getRestTemplate(){
 		RestTemplate restTemplate = new RestTemplate();
 		return restTemplate;
+	}
+
+	@Bean
+	ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder){
+		ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+		objectMapper.setPropertyNamingStrategy(com.fasterxml.jackson.databind.PropertyNamingStrategy.SNAKE_CASE);
+		return objectMapper;
 	}
 
 }
