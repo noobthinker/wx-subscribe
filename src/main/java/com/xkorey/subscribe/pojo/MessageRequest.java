d3bdd1359430e23a194ed002a4b3c1afdbb9798b
xkorey <xkorey@gmail.com>
2019-07-30 22:52:28 +0800

    message

--- src/main/java/com/xkorey/subscribe/pojo/MessageRequest.java
+++ src/main/java/com/xkorey/subscribe/pojo/MessageRequest.java
@@ -2,17 +2,24 @@ package com.xkorey.subscribe.pojo;
 
 import lombok.Data;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
 
-@XmlRootElement
+@XmlRootElement(name = "xml")
 @Data
-public class MessageRequest {
-
+public class MessageRequest implements Serializable {
+    @XmlElement
     private String ToUserName;
+    @XmlElement
     private String FromUserName;
+    @XmlElement
     private Long CreateTime;
+    @XmlElement
     private String MsgType;
+    @XmlElement
     private String Content;
+    @XmlElement
     private Long MsgId;
 
 }
