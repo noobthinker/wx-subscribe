69a1fcacdcefb60869c470240edd47b38f7eede9
xkorey <xkorey@gmail.com>
2019-07-30 21:43:20 +0800

    json转换设置

--- src/main/java/com/xkorey/subscribe/pojo/TokenResult.java
+++ src/main/java/com/xkorey/subscribe/pojo/TokenResult.java
@@ -1,9 +1,12 @@
 package com.xkorey.subscribe.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class TokenResult {
+    @JsonProperty("access_token")
     String accessToken;
+    @JsonProperty("expires_in")
     Integer expiresIn;
 }
