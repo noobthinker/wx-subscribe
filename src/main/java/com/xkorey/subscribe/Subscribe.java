373ad7d8df7410764214cbf0b0f3de0be7c1b742
xkorey <xkorey@gmail.com>
2019-07-30 19:37:41 +0800

    init

--- src/main/java/com/xkorey/subscribe/Subscribe.java
+++ src/main/java/com/xkorey/subscribe/Subscribe.java
@@ -1,14 +1,20 @@
 package com.xkorey.subscribe;
 
 
+import com.xkorey.subscribe.pojo.Signature;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class Subscribe {
 
     @RequestMapping({"/","/index.html","index.do"})
-    public String index(){
+    public String index(Signature signature){
+        if(null!=signature){
+            log.info("str {}",signature.getEchostr());
+        }
         return "";
     }
 }
