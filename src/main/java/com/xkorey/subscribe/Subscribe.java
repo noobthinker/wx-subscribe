ca3fe1400641254ef7d05762fc4cd60465a2ded5
xkorey <xkorey@gmail.com>
2019-07-30 19:49:48 +0800

    校验设置

--- src/main/java/com/xkorey/subscribe/Subscribe.java
+++ src/main/java/com/xkorey/subscribe/Subscribe.java
@@ -3,6 +3,7 @@ package com.xkorey.subscribe;
 
 import com.xkorey.subscribe.pojo.Signature;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class Subscribe {
 
+    @Value("${signature.flag}")
+    boolean signatureFlag;
+
+
     @RequestMapping({"/","/index.html","index.do"})
     public String index(Signature signature){
         if(null!=signature){
-            if(null!=signature.getEchostr()){
+            if(null!=signature.getEchostr() && signatureFlag){
                 return signature.getEchostr();
             }
         }
