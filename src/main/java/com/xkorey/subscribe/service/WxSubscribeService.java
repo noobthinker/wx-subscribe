b05405063630cb92ff0d444f5122a69ca8c0c9a8
xkorey <xkorey@gmail.com>
2019-07-30 21:31:37 +0800

    校验设置

--- src/main/java/com/xkorey/subscribe/service/WxSubscribeService.java
+++ src/main/java/com/xkorey/subscribe/service/WxSubscribeService.java
@@ -42,8 +42,10 @@ public class WxSubscribeService implements IService {
 
 
     TokenResult newToken(){
+        ResponseEntity<String> sResult =  restTemplate.getForEntity(apiTokenUrl, String.class);
         ResponseEntity<TokenResult> result = restTemplate.getForEntity(apiTokenUrl, TokenResult.class);
         log.info("wx result : ",result);
+        log.info("string result : {}",sResult);
         if(result.getStatusCode()== HttpStatus.OK){
             return result.getBody();
         }
