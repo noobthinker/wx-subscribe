1f7963c1a06928c7f92bfaec9b4613d28eb943a1
xkorey <xkorey@qq.com>
2019-08-04 03:46:05 +0800

    staff

--- src/main/java/com/xkorey/subscribe/pojo/WxNewsReply.java
+++ src/main/java/com/xkorey/subscribe/pojo/WxNewsReply.java
@@ -3,10 +3,14 @@ package com.xkorey.subscribe.pojo;
 
 import lombok.Data;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
-
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "Articles")
 @Data
 public class WxNewsReply {
 
